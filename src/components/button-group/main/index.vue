524aeed38ba5a480b7dffd7aecf6daf427ec5526
郑鹏飞 <zhengpengfei@elephx.io>
2024-02-02 16:17:32 +0800

    perf: 升级 ButtonGroup 组件

---
+++ src/components/button-group/main/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="fly-button-group">
+    <slot />
+  </div>
+</template>
+<script>
+export default {
+  name: 'FlyButtonGroup',
+  props: {
+
+  }
+}
+</script>
+
+<style lang="scss">
+@import '../../button/main/index.scss';
+</style>
